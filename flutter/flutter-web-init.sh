5d3dd30cf4d3079847426b43176e638d7bab0435
Takayuki Uchida <takuchidamh41@gmail.com>
2021-01-18 22:58:20 +0900

    init shell delete dir process

--- flutter/flutter-web-init.sh
+++ flutter/flutter-web-init.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 cd ${FLUTTER_WEB_VOLUME}
+if [ -e ./${APPLICATION_NAME} ]; then
+    rm -rf ${APPLICATION_NAME}
+fi
+
 if [ -z $FLUTTER_WEB_GIT_REPOSITORY ]; then
     flutter create ${APPLICATION_NAME}
 else
